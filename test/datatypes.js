f4768d86ed2e2676b0bde9e3ec9d8ebda1058c03
saifdawar <133679893+saifdawar@users.noreply.github.com>
2025-02-19 21:26:39 +0000

    File update

---
+++ test/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser 
+//code readability should be priority
+
+/* number => 2 to the power 53
+ bigInt 
+ String = ""
+ null ek standalone value hay
+ undefined ka matlab usko value nhi di gayi abhi
+ symbol uniqueness k liye use hota hay
+ object 
+ */
+console.log( typeof "Saif")
+console.log( typeof null)
+console.log( typeof undefined)
---
+++ test/variable.js
@@ -0,0 +1,8 @@
+const Id = 1234556
+let name = "saif"
+var age ="24"
+pass = "112233"
+console.log(Id);
+console.table([Id, name, age , pass])
+
+//VAR should not be used
